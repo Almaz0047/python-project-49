f0dbbe83e606306fb6a29349e2b8db3964bb26c0
Almaz <keiiia0047@gmail.com>
2023-12-07 16:06:18 +0300

    some small change

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,28 +1,27 @@
-import random, prompt
-import cli
+import random, prompt, cli
 
-def  main():
+
+def main():
     usname = cli.welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
     correct = 1
     while True:
-        number = random.randint(1,100)
+        number = random.randint(1, 100)
         print(f'Question: {number}')
-        usanswer = prompt.string('Your answer: ') 
+        usanswer = prompt.string('Your answer: ')
         if correct == 3:
             print(f'Congratulations, {usname}!')
             break
-        elif number % 2 == 0 and usanswer == 'yes':
+        elif number % 2 == 0 and usanswer == 'yes' or number % 2 != 0 and usanswer == 'no':
             print('Correct!')
             correct += 1
         elif number % 2 == 0 and usanswer != 'yes':
-            print(f"Answer '{usanswer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again,{usname}!")
+            print(f"Answer '{usanswer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, {usname}!")
             break
-        elif number % 2 != 0 and usanswer == 'no':
-            print('Correct!')
-            correct += 1
         elif number % 2 != 0 and usanswer != 'no':
             print(f"Answer '{usanswer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {usname}!")
             break
 
 
+if __name__ == '__main__':
+    main()
