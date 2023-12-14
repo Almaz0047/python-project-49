4cdedf01a6020fd1d000c7cfb18c0bd8e74a77a5
Almaz <keiiia0047@gmail.com>
2023-12-15 01:17:04 +0300

    new-made logic

--- brain_games/logic.py
+++ brain_games/logic.py
@@ -1,23 +1,23 @@
 import brain_games.cli
 import prompt
 
+SCORE = 3
+
 
 def start(game):
     usname = brain_games.cli.welcome_user()
-    correct = 1
+    index = 0
     print(game.TASK)
-    while True:
+    while index < SCORE:
         correct_answer, question = game.engine()
         print(f'Question: {question}')
         usanswer = prompt.string('Your answer: ')
-        if correct == 3:
-            print(f'Congratulations! {usname}')
-            break
-        elif usanswer == correct_answer:
+        if usanswer == correct_answer:
             print('Correct!')
-            correct += 1
         else:
             print(f"'{usanswer}' is wrong answer ;(. "
                   f"Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {usname}!")
             return
+        index += 1
+    print(f"Congratulations, {usname}!")
